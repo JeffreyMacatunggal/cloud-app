692746849bd9afa97093d1e3e0556e4d1bc2834f
phcodesage <rechceltoledo@gmail.com>
2023-10-26 15:51:17 +0800

    initial commit

--- company-app-frontend/src/components/CompanyForm.jsx
+++ company-app-frontend/src/components/CompanyForm.jsx
@@ -30,33 +30,41 @@ const CompanyForm = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <div className="max-w-md bg-white p-8 rounded-xl shadow-2xl dark:bg-gray-800 transition-transform transform hover:scale-105">
-        <h2 className="text-2xl font-semibold mb-6 text-center dark:text-white">Add a Company</h2>
-        <form onSubmit={handleSubmit}>
+    <div className="max-w-md mx-auto mt-10 bg-white p-8 rounded shadow-md">
+      <header className="mb-6">
+        <h1 className="text-3xl font-bold text-center">Company Management System</h1>
+      </header>
+      
+      <h2 className="text-2xl font-semibold mb-6">Add a Company</h2>
+      <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label className="block mb-1 text-sm font-medium text-gray-600 dark:text-gray-300" htmlFor="name">Company Name</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} placeholder="Company Name" required className="appearance-none bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          <label className="block text-sm font-medium mb-2" htmlFor="name">Company Name</label>
+          <input type="text" name="name" placeholder="Company Name" value={formData.name} onChange={handleChange} required className="w-full p-2 border rounded" />
         </div>
-
+        
         <div className="mb-4">
-          <label className="block mb-1 text-sm font-medium text-gray-600 dark:text-gray-300" htmlFor="address">Address</label>
-          <input type="text" id="address" name="address" value={formData.address} onChange={handleChange} placeholder="Address" required className="appearance-none bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          <label className="block text-sm font-medium mb-2" htmlFor="address">Address</label>
+          <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} required className="w-full p-2 border rounded" />
         </div>
-
+        
         <div className="mb-4">
-          <label className="block mb-1 text-sm font-medium text-gray-600 dark:text-gray-300" htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required className="appearance-none bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          <label className="block text-sm font-medium mb-2" htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required className="w-full p-2 border rounded" />
         </div>
-
+        
         <div className="mb-6">
-          <label className="block mb-1 text-sm font-medium text-gray-600 dark:text-gray-300" htmlFor="phone">Phone</label>
-          <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone" required className="appearance-none bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          <label className="block text-sm font-medium mb-2" htmlFor="phone">Phone</label>
+          <input type="text" name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} required className="w-full p-2 border rounded" />
         </div>
-
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-transform transform hover:scale-105">Add Company</button>
+        
+        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+          Add Company
+        </button>
       </form>
-      </div>
+
+      <footer className="mt-10">
+        <p className="text-center text-sm text-gray-500">© 2023 Your Company Name. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
